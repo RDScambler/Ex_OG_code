429448772d7b2baab54e21c375297b6f489c86f1
Ross Scambler <scambler@hotmail.co.uk>
2019-11-28 17:30:55 +0000

    Completed work on settest.py. The code now runs through every pairwise combination of groups (and each single group) in genome dataset.
    Major changes have been made to findgroup2.py.
    The group query inputs have been removed and code from settest.py has been incorporated in its place.
    findgroup2.py now automatically searches through the dataset, returning exclusive OGs for each pairwise combination (and single group).

--- findgroup2.py
+++ findgroup2.py
@@ -1,12 +1,11 @@
+
 import glob
 import re
 
-j = 0
-query = []
+
 code_map = {}
 group_names = []
-nameError = "Incorrect name: please check spelling."
-genome = open('C:\\Users\\scamb\\Documents\\UoB MSc\\Genome_data\\OG_4up_arb-fal.tar\\OG_4up_arb-fal\\Group outputs\\genomeOutput.txt', 'w')
+all_sets = []
 
 with open('Eukaryote_codes.txt') as f:
     for line in f:
@@ -15,46 +14,40 @@ with open('Eukaryote_codes.txt') as f:
         if fields[1] not in group_names:
             group_names.append(fields[1])
 
-while True:
-	print('First group query:')                             #Requesting a group
-	query1 = input()
-	if query1 not in group_names:
-		print(nameError)
-	else:
-		break
-
-query.append(query1)
-
-while True:
-	print('Second group query (or enter to skip):')         #Allows second input to be optional
-	query2 = input()
-	if query2 == '':
-                break
-	elif query2 not in group_names:
-		print(nameError)
-	else:
-		query.append(query2)
-		break
-
-to_parse = glob.glob('*.fal')				#Searches through all .fal files.
-
-for file in to_parse:
-    groups_present = []
-    with open(file) as f:
-        for line in f:
-            if line.startswith('>'):
-                fields = re.split('_', line)            #Separates sp. code
-                species_code = fields[0][1:]		# Removes '>'
-                for i in code_map:                      #Linking sp. code to group
-                    group = code_map[species_code]
-                    if group not in groups_present:     #Adding to group array if new group
-                        groups_present.append(group)
-    if groups_present == query:                         #Adds file to output if it matches query
-        genomewrite = genome.write(f'{file}\n')
-        j += 1
-    elif groups_present == query[::-1]:                 #Accounts for variation in the list order
-        genomewrite = genome.write(f'{file}\n')
-        j += 1
-
-genomewrite = genome.write(f'Shared gene families: {j}')
-genome.close()
+for j in range(len(group_names)):
+        for i in range(len(group_names)):                       # Iterates over every element of group_name by every other element
+                query = group_names[j], group_names[i]
+                file1 = '_'.join(query)				# Formats query for filenaming purposes
+                file1 += '_output'
+                if group_names[j] != group_names[i]:
+                        group_set = set(query)                  # Creates set, disregarding the order.
+                        x = 0					# x value necessary for the if else statement (lines 40-45).
+                else:                                           # Modifies group_set if j == i to only have 1 element..
+                        group_set = [group_names[j]]		# Creates array. Single element array will match with (unconverted) single element instances of groups_present.
+                        x = 1
+                if group_set not in all_sets:    		# Excludes sets which already exist.
+                        all_sets.append(group_set)
+                        filename = "%s.txt" % file1		# Formatting filename.
+                        genome = open(filename, 'w')
+                        to_parse = glob.glob('*.fal')		# Searches through all .fal files.
+                        k = 0					# Counter (giving total exclusive OGs for set).
+                        for file in to_parse:
+                                groups_present = []
+                                with open(file) as f:
+                                        for line in f:
+                                                if line.startswith('>'):
+                                                        fields = re.split('_', line)            # Separates sp. code
+                                                        species_code = fields[0][1:]		# Removes '>'
+                                                        for i in code_map:                      # Linking sp. code to group
+                                                                group = code_map[species_code]
+                                                                if group not in groups_present:     	# Adding to group array if new group
+                                                                        groups_present.append(group)
+                                if x == 0:								# Converts array to set (so order is irrelevant).
+                                        set_present = set(groups_present)
+                                else:									# groups_present remains as array to match with single element instances of group_set.
+                                        set_present = groups_present
+                                if group_set == set_present:
+                                        genomewrite = genome.write(f'{file}\n') 	# Adds file to output if it matches query
+                                        k += 1
+                        genomewrite = genome.write(f'Shared gene families: {k}')
+                        genome.close()
--- settest.py
+++ settest.py
@@ -12,10 +12,19 @@ with open('Eukaryote_codes.txt') as f:
             group_names.append(fields[1])
 
 for j in range(len(group_names)):
-	for i in range(len(group_names)):			# Iterates over every element of group_name by every other element
+	for i in range(len(group_names)):			# Iterates over every element of group_name by every other element.
 		query = group_names[j], group_names[i]
-		group_set = set(query)					# Converts list to set, disregarding the order.
-		if group_set not in all_sets and len(group_set) > 1:	# Excludes single element sets and sets which already exist.
+		x = '_'.join(query)				# Joins query into a single string for filenaming purposes.
+		x += '_output'
+		if group_names[j] != group_names[i]:
+			group_set = set(query)			# Converts list to set, disregarding the order.
+		else:						# Modifies group_set if j == i.
+			group_set = group_names[j]
+		if group_set not in all_sets:			# Excludes already-existing sets from further analysis.
 			all_sets.append(group_set)
+			print(x)
 
 print(all_sets)
+
+
+
