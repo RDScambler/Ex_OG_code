e5d68fc709232a9e0134e1d3d88022bb2c2a4819
Ross Scambler <scambler@hotmail.co.uk>
2020-02-07 14:13:01 +0000

    Added parseSeq.py. This is a prototype parsing script to move desired seqs from dataset into eggnog input file.

---
+++ parseSeq.py
@@ -0,0 +1,14 @@
+						# *** Template for parsing seqs from .fals into input for eggnog. (Multiple input files do not seem to be possible). ***
+output = open("eggnog_toannotate.fal", "w")
+
+with open("Metamonads_Malawimonadidae_Other_Ancyromonadida.txt") as f:		# Can be adjusted depending on target taxa.
+	for line in f:
+		if "Shared" in line:
+			pass
+		else:
+			file = line.rstrip("\n\r")				# .rstrip is handy for selectively removing elements of a string.
+			with open(file) as f:
+				for line in f:
+					outputWrite = output.write(line)	# Writes out every sp code and corresponding seq ready for eggnog analysis.
+				newline = output.write("\n")			# Separates seqs from different files.
+output.close()
