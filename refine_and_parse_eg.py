5a4fdbd6c788c75612b5abf0df034e32bb5629d7
Ross Scambler <scambler@hotmail.co.uk>
2020-10-03 13:10:46 +0100

    Tweaked refine_and_parse_eg.py Updated README.md

--- refine_and_parse_eg.py
+++ refine_and_parse_eg.py
@@ -7,47 +7,60 @@
 			# Summary stats now added, and query name requested for convenience.
 import re
 import group
-import full_og_list
 
-query = input("Enter the name of your query (e.g. Metamonads_Discoba): \n")
+# Ask user for query taxa.
+# Append to a list all the uniquely shared OGs between the query taxa using count_ogs.
+while True:
+	try:
+		query = input("Enter query taxa (e.g. Metamonads_Discoba): \n")
+		preanno_full_list = group.count_ogs("/mnt/c/Users/scamb/Documents/uob_msc/Genome_data/OG_arb-fal/new_outputs/" + query + "_output.txt")
+		if preanno_full_list is not None:
+			break
+	except FileNotFoundError as e:
+		print(f'{e} not found. Please enter a valid query.')
+		
+# Define code map.
+ortho = group.OrthogroupSearch('codes_alt_18')
 output = open(query + "_refined_eg.txt", "w")
-
-codes = group.alt_codes()
 og_list = []
-preanno_full_list = full_og_list.full_og_list("/mnt/c/Users/scamb/Documents/uob_msc/Genome_data/OG_arb-fal/new_outputs/" + query + "_output.txt")
-species_list = []
-sp_label_regex = "(\w{,8})_\w*[tn|gn]\d+_(\w+\-\w+)_(OG\d+)"					# Splits up the first entry of output file (these are not tab-separated).
 
-with open(query + ".emapper.annotations") as f:							# Filename must be edited as necessary.
+# Split up the first entry of output file (these are not tab-separated).
+sp_label_regex = r"(\w{,8})_\w*[tn|gn]\d+_(\w+\-\w+)_(OG\d+)"					
+
+with open(query + ".emapper.annotations") as f:	
 	for line in f:
 		res = re.search(sp_label_regex, line)
 		if res:
 			sp_code = res.group(1)
-			sp_name = res.group(2)
-			if sp_name not in species_list:						# Making this list to conveniently get a list of all ventral-groovers.
-				species_list.append(sp_name)					# Worth noting this may exclude some taxa only captured in pairwise analysis.
+			sp_name = res.group(2)					
 			og = res.group(3)
-			eugroup = codes[sp_code]						# Eukaryote group is derived from the codes dict.
-			fnames = eugroup.ljust(20) + sp_name.ljust(30)				# .ljust formats for appearance.
+
+			# Eukaryote group is derived from the codes dict.
+			eugroup = ortho.code_map()[sp_code]
+
+			# .ljust formats for appearance.						
+			fnames = eugroup.ljust(20) + sp_name.ljust(30)				
 			tabline = re.split("\t", line.strip())
 			e = tabline[2].ljust(15)
-			ftax_hit = tabline[4].replace(" ", "_").ljust(40)			# Removing whitespace from between tax_hit strings (for ease of future parsing).
+
+			# Remove whitespace from between tax_hit strings (for future parsing).
+			ftax_hit = tabline[4].replace(" ", "_").ljust(40)			
 			gene_id = tabline[5].ljust(10)
 			tax_scope = tabline[17].ljust(20)
 			func_cat = tabline[-2]
 			text_description = tabline[-1]
-			if og not in og_list:							# Appends ogs to og_list for the purposes of identifying new ogs and inserting \n accordingly.
+
+			# Append ogs to og_list for the purposes of identifying new ogs.
+			if og not in og_list:							
 				og_list.append(og)
 				outputWrite = output.write("\n")
 			outputWrite = output.write(f"{og}\t{fnames}{e}{ftax_hit}{tax_scope}{gene_id}{func_cat}\t{text_description}\n")
 
 output.close()
 
-# Here a cutoff point is requested.
-# This can be customised as required.
+# Here a cutoff point is requested, this can be customised as required.
 # The proportion defines how many bacterial sequences can make up the eukaryote/archaea output file.
 # I.e what level is being considered as homologous rather than a contaminant.
-
 while True:
 	cutoff = float(input("What is the bacterial cutoff point for orthogroups (as a proportion of the group's sequences)?\n"))
 	if cutoff < 1 and cutoff > 0:
@@ -65,12 +78,18 @@ bac_list = []
 for og in og_list:
 	j = 0
 	tax_list = []
-	while j < 2:												# While loop is necessary to iterate over the file twice.
-		i = 0												# The first loop appends all taxa in og to tax_list.
-		with open(query + "_refined_eg.txt") as f:							# The second compares the frequency of Bacterial seqs to the cutoff.
+
+	# While loop necessary to iterate over the file twice.
+	# The first loop appends all taxa in og to tax_list.
+	# The second compares the frequency of Bacterial seqs to the cutoff.
+	while j < 2:												
+		i = 0												
+		with open(query + "_refined_eg.txt") as f:							
 			if j < 1:
 				for line in f:
-					if og in line:								# Matches each og in turn.
+
+					# Match each og in turn.
+					if og in line:								
 						spline = re.split(r"\s+", line.strip(), maxsplit=6)
 						tax = spline[5]
 						tax_list.append(tax)
@@ -78,25 +97,34 @@ for og in og_list:
 				for taxon in tax_list:
 					if taxon == "Bacteria":
 						i += 1
-				if i < cutoff * len(tax_list):							# If below the cutoff, ogs are written to the euk/arc output file.
+
+				# If below the cutoff, ogs are written to the 'ean' output file.		
+				if i < cutoff * len(tax_list):							
 					for line in f:
 						if og in line:
 							outputWrite = ean_output.write(line)
-							if og not in euk_arc_list:				# For the purposes of collecting summary stats.
+
+							# For the purposes of collecting summary stats.
+							if og not in euk_arc_list:				
 								euk_arc_list.append(og)
-				else:										# Otherwise they are written to the bacterial output file.
+
+				# Otherwise they are written to the bacterial output file.				
+				else:										
 					for line in f:
 						if og in line:
 							outputWrite = bac_output.write(line)
 							if og not in bac_list:
 								bac_list.append(og)
-		j += 1												# j ensures while loop occurs twice.
+		
+		# j ensures while loop occurs twice.
+		j += 1												
 
 bac_output.close()
 
-# Identifies ogs not analysed by eggnog.
-# Accesses and parses out relevant sp and og data from pre-annotation file.
+# Identify ogs not analysed by eggnog.
+# Access and parse out relevant sp and og data from pre-annotation file.
 # non-hits are appended to the ean_output file.
+# It is necessary to parse data from "xxxxxx_toannotate.fal' file since sp. name and code data are here.
 for og in preanno_full_list:
 	if og not in og_list:
 		with open("/mnt/c/Users/scamb/Documents/uob_msc/Genome_data/eggnog-mapper/eg_" + query + "_toannotate.fal") as f:
@@ -106,9 +134,7 @@ for og in preanno_full_list:
 					if res:
 						sp_code = res.group(1)
 						sp_name = res.group(2)
-						if sp_name not in species_list:
-							species_list.append(sp_name)
-						eugroup = codes[sp_code]
+						eugroup = ortho.code_map()[sp_code]
 						fnames = eugroup.ljust(20) + sp_name
 						no_hits_outputWrite = ean_output.write(f"{og}\t{fnames}\n")
 
@@ -137,6 +163,6 @@ print(total)
 print(tot_minus)
 print(tot_euk)
 
-outputWrite = output.write(f"\n{stats}{per_bac}\n{per_euk}\n{per_overall}\n{total}\n{tot_minus}\n{tot_euk}")
+output.write(f"\n{stats}{per_bac}\n{per_euk}\n{per_overall}\n{total}\n{tot_minus}\n{tot_euk}")
 
 output.close()
